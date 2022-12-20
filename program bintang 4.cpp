a562f69bd8b04fd47693ddc3a7fc7c57a653241b
Ghina Aqli Syahirah <114642992+ghinaaqli@users.noreply.github.com>
2022-12-20 21:46:53 +0700

    Tugas 5 yg nitip di repository Tugas 6

---
+++ program bintang 4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n, s, i, j;
+      cout << "Masukkan jumlah baris: ";
+      cin >> n;
+    for (i = n; i >= 1; i--) {
+        for (s = i; s < n; s++)
+            cout << " ";
+        for (j = 1; j <= i; j++)
+            cout << "* ";
+            cout << endl;
+      }
+      return 0;
+}
